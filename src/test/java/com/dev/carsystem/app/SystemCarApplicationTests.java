4885ddfe1a8a7bfdfb3cd7c617dcf88d2724f681
Erick Estrada <erickestradatech@gmail.com>
2021-10-31 04:54:21 +0000

    starting project

---
+++ src/test/java/com/dev/carsystem/app/SystemCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.carsystem.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
